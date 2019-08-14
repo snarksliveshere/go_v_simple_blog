521fac5f69931c1afd083ee806ed3f51c5017dd8
snarksliveshere <snarksliveshere@gmail.com>
2019-08-14 20:53:53 +0300

    move to martini render

--- main.go
+++ main.go
@@ -3,69 +3,100 @@ package main
 import (
 	"blog/models"
 	"fmt"
-	"html/template"
 	"net/http"
+
+	"github.com/codegangsta/martini"
+	"github.com/martini-contrib/render"
 )
 
 var posts map[string]*models.Post
+var counter int
 
 func main() {
 	posts = make(map[string]*models.Post, 0)
 
-	http.Handle("/assets/", http.StripPrefix("/assets/",http.FileServer(http.Dir("./assets/"))))
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/write", writeHandler)
-	http.HandleFunc("/edit", editHandler)
-	http.HandleFunc("/SavePost", savePostHandler)
-	http.HandleFunc("/delete", deleteHandler)
-	http.ListenAndServe(":3000", nil)
+	m := martini.Classic()
+
+	m.Use(render.Renderer(render.Options{
+		Directory:  "templates",
+		Layout:     "layout",
+		Extensions: []string{".tmpl", ".html"},
+		//Funcs:           nil,
+		//Delims:          render.Delims{},
+		Charset:    "UTF-8",
+		IndentJSON: true,
+		//IndentXML:       false,
+		//PrefixJSON:      nil,
+		//PrefixXML:       nil,
+		//HTMLContentType: "",
+	}))
+
+	counter = 0
+	m.Use(func(r *http.Request) {
+		if r.URL.Path == "/write" {
+			counter++
+		}
+	})
+
+	staticOptions := martini.StaticOptions{Prefix: "assets"}
+	m.Use(martini.Static("assets", staticOptions))
+	m.Get("/", indexHandler)
+	m.Get("/write", writeHandler)
+	m.Get("/edit", editHandler)
+	m.Post("/SavePost", savePostHandler)
+	m.Get("/delete", deleteHandler)
+	m.Get("/404", notFoundHandler)
+
+	m.Run()
 }
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
-	if err != nil {
-		fmt.Fprintf(w, err.Error())
-	}
-	fmt.Println(posts)
-	t.ExecuteTemplate(w, "index", posts)
+func notFoundHandler(rnd render.Render) {
+	rnd.HTML(200, "404", posts)
+}
+func indexHandler(rnd render.Render) {
+	fmt.Println(counter)
+	rnd.HTML(200, "index", posts)
 }
 
-func writeHandler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("templates/write.html", "templates/header.html", "templates/footer.html")
-	if err != nil {
-		fmt.Fprintf(w, err.Error())
-		return
-	}
-
-	t.ExecuteTemplate(w, "write", nil)
+func writeHandler(rnd render.Render) {
+	rnd.HTML(200, "write", nil)
 }
 
-func deleteHandler(w http.ResponseWriter, r *http.Request) {
+//func writeHandler(w http.ResponseWriter, r *http.Request) {
+//	t, err := template.ParseFiles("templates/write.html", "templates/layout.html", "templates/footer.html")
+//	if err != nil {
+//		fmt.Fprintf(w, err.Error())
+//		return
+//	}
+//
+//	t.ExecuteTemplate(w, "write", nil)
+//}
+
+func deleteHandler(rnd render.Render, r *http.Request) {
 	id := r.FormValue("id")
 	if id == "" {
-		http.NotFound(w, r)
+		rnd.Redirect("/404")
+		//http.NotFound(w, r)
 	}
 	delete(posts, id)
-	http.Redirect(w, r, "/", 302)
+	rnd.Redirect("/")
+	//http.Redirect(w, r, "/", 302)
 }
 
-func editHandler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("templates/write.html", "templates/header.html", "templates/footer.html")
-	if err != nil {
-		fmt.Fprintf(w, err.Error())
-		return
-	}
-
+func editHandler(rnd render.Render, r *http.Request) {
 	id := r.FormValue("id")
 	post, found := posts[id]
 	if !found {
-		http.NotFound(w, r)
+		// redirect to 404
+		rnd.Redirect("/404")
+		return
+		//http.NotFound(w, r)
 	}
-
-	t.ExecuteTemplate(w, "write", post)
+	rnd.HTML(200, "write", post)
+	//t.ExecuteTemplate(w, "write", post)
 }
 
-func savePostHandler(w http.ResponseWriter, r *http.Request) {
+func savePostHandler(rnd render.Render, r *http.Request) {
 	id := r.FormValue("id")
 	title := r.FormValue("title")
 	content := r.FormValue("content")
@@ -81,6 +112,24 @@ func savePostHandler(w http.ResponseWriter, r *http.Request) {
 		posts[post.Id] = post
 	}
 
-	http.Redirect(w, r, "/", 302)
+	rnd.Redirect("/")
 }
 
+//func savePostHandler(w http.ResponseWriter, r *http.Request) {
+//	id := r.FormValue("id")
+//	title := r.FormValue("title")
+//	content := r.FormValue("content")
+//	var post *models.Post
+//
+//	if id != "" {
+//		post = posts[id]
+//		post.Title = title
+//		post.Content = content
+//	} else {
+//		id = GenerateId()
+//		post := models.NewPost(id, title, content)
+//		posts[post.Id] = post
+//	}
+//
+//	http.Redirect(w, r, "/", 302)
+//}
